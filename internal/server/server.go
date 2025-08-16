d9577a6030782d1aa4f43ceb2e6deb4c0d14c6ad
Tushar Anand <tusharannand@gmail.com>
2025-08-16 17:20:47 +0530

    basic response done upto c7l2
    Signed-off-by: Tushar Anand <tusharannand@gmail.com>

---
+++ internal/server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+type Server struct {
+	closed bool
+}
+
+func runConnection(s *Server,conn io.ReadWriteCloser)  {
+	out := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello World!`")
+	conn.Write(out)
+	conn.Close()
+}
+
+func runServer(s *Server, listner net.Listener) {
+		for {
+			conn,err := listner.Accept()
+			if s.closed {
+				return
+			}
+			if err != nil {
+				return
+			}
+		go runConnection(s,conn)
+		}
+}
+
+func Serve(port uint16) (*Server,error){
+	listner,err := net.Listen("tcp",fmt.Sprintf(":%d",port)) 
+	if err != nil {
+		return nil,err
+	}
+	server := &Server{closed: false}
+	go runServer(server,listner)
+
+	return server,nil
+}
+
+func (s *Server) Close() error {
+	s.closed = true
+	return nil
+}
